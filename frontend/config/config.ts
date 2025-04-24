e4d5ebca05e8e8cd726ade39769519c2051ad1fa
Florent Azonnoudo <florent@weekshipper.com>
2025-04-24 11:58:27 +0000

    fix: update apiServer URL to correct endpoint

--- frontend/config/config.ts
+++ frontend/config/config.ts
@@ -1,5 +1,5 @@
 const Config = {
-  apiServer: process.env.NEXT_PUBLIC_BACKEND_ORIGIN || 'https://intranet.arcop.com:8002',
+  apiServer: process.env.NEXT_PUBLIC_BACKEND_ORIGIN || 'http://46.202.129.20:8002',
 
   // set 'NEXT_PUBLIC_IS_DEMO=true' in frontend/.env
   isDemoSite: process.env.NEXT_PUBLIC_IS_DEMO === 'true' || false,
